6631392b4cfc991f5f01b5e42c4bf6b030d66e33
Jacob Mosley <jmosle13@uncc.edu>
2022-11-05 19:13:39 -0400

    adding and displaying comments functionality

--- flask06.py
+++ flask06.py
@@ -8,6 +8,8 @@ from models import User as User
 from forms import RegisterForm
 from flask import session
 from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 # imports
 import os  # os is used to get environment variables IP & PORT
@@ -51,10 +53,12 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='jmosle13@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-
-    return render_template("note.html", note=my_note, user=a_user)
+    if session.get('user'):
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        form = CommentForm()
+        return render_template("note.html", note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/new', methods=['GET', 'POST'])
@@ -167,6 +171,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
